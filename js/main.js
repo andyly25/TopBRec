2e8f9c48121d34e9b8f2f9518cfc104f7e85c404
Andy Ly <andywindsong@gmail.com>
2018-06-13 16:18:07 -0700

    Feature: dropdown menu + text input on submit worked

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 const nytimesKey = config.NYT_KEY;
 // const googleBooksKey = config.GOOGLE_BOOKS_KEY;
 const NYT_BOOKS_ENDPOINT = 'https://api.nytimes.com/svc/books/v3/lists.json';
-const GOOGLE_BOOKS_ENDPOINT = 'https://www.googleapis.com/books/v1/volumes'
+const GOOGLE_BOOKS_ENDPOINT = 'https://www.googleapis.com/books/v1/volumes';
 
 function initPage () {
   fetch(`${NYT_BOOKS_ENDPOINT}?list-name=hardcover-fiction&api-key=${nytimesKey}`, {
@@ -17,39 +17,60 @@ function initPage () {
       console.log(json);
     })
     .catch((error) => {
+      // in the case of hitting the rate limit... we'll use an archive
       console.log(`NYT API Error: Search not found: ${error}`);
+      updateBestSellers(nytimesArchive);
     });
 }
 
+// $(document).ready(function () {
+//   let option = $('#searchField').find('option:selected').val();
+//   $('#searchField').change(function () {
+//     option = $(this).find('option:selected').val();
+//     console.log(option);
+//   });
+// });
+
 function handleForm () {
   const bookSearchForm = $('form[name=book-search');
-  const bookField = $('input[name=input-book]');
-  const authorField = $('input[name=input-author]');
-  const genreField = $('input[name=input-genre]');
+  // const bookField = $('input[name=input-book]');
+  // const authorField = $('input[name=input-author]');
+  // const genreField = $('input[name=input-genre]');
+  const searchInput = $('input[name=user-input');
+  let option = $('#searchField').find('option:selected').val();
+  $('#searchField').change(function () {
+    option = $(this).find('option:selected').val();
+    console.log(option);
+  });
 
   bookSearchForm.on('submit', (e) => {
     e.preventDefault();
     // get user values inputted
-    const book = bookField.val();
-    const author = authorField.val();
-    const genre = genreField.val();
-
-    // pass along with Google endpoint
-    fetchBookData(GOOGLE_BOOKS_ENDPOINT, book, author, genre);
+    // const book = bookField.val();
+    // const author = authorField.val();
+    // const genre = genreField.val();
+    let userSearch = searchInput.val();
+    userSearch = userSearch.replace(/\s+/g, '+').toLowerCase();
+    // maybe append &max-results=20 at end
+    console.log(`${GOOGLE_BOOKS_ENDPOINT}?q=${option}:${userSearch}`);
 
     // reset the input
-    resetFields(bookField, authorField, genreField);
+    resetFields(searchInput);
+
+    // pass along with Google endpoint
+    // fetchBookData(GOOGLE_BOOKS_ENDPOINT, book, author, genre);
+    fetchBookData(GOOGLE_BOOKS_ENDPOINT, option, userSearch);
   });
 }
 
-function fetchBookData (baseURL, book, author, genre) {
+function fetchBookData (baseURL, option, searchTerm) {
   // make a url by concat endpoints together
+  $('#best-seller-titles').append(`<p>Testing: ${baseURL}?q=${option}:${searchTerm}</p>`);
 }
 
-function resetFields (books, author, genre) {
-  books.val('');
-  author.val('');
-  genre.val('');
+function resetFields (userSearch) {
+  userSearch.val('');
+  $('#best-seller-titles').empty();
 }
 
 function updateBestSellers (nytimesBestSellers) {
@@ -102,4 +123,8 @@ function updateCover (id, isbn) {
     });
 }
 
-$(initPage);
+$(() => {
+  initPage();
+  handleForm();
+});
+
